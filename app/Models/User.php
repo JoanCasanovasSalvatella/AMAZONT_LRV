d70ada565313d2f8e20b8bf3d56b5d2ce1059b98
aroqiler <156545745+aroqiler@users.noreply.github.com>
2025-03-12 16:44:10 +0100

    ROL Y ADRESS AÑADIDO

--- app/Models/User.php
+++ app/Models/User.php
@@ -22,8 +22,10 @@ class User extends Authenticatable
     protected $fillable = [
         'name',
         'email',
+        'adress',
         'email_verified_at',
         'password',
+        'rol',
         'remember_token'
     ];
 
